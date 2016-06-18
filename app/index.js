9e04c370b21914e8b233927a53ece96a90626fc9
Corey Ellis <corey@coreyellis.me>
2016-06-17 21:17:25 -0500

    Update index.js

--- app/index.js
+++ app/index.js
@@ -10,10 +10,7 @@ var WpMakeGenerator = yeoman.generators.Base.extend({
 		// replace it with a short and sweet description of your generator
 		this.log(chalk.magenta('Invoke a subgenerator to get started!'));
 		this.log("Available Modules:");
-		this.log(chalk.green("\tyo wp-make:plugin"));
 		this.log(chalk.green("\tyo wp-make:theme"));
-		this.log(chalk.green("\tyo wp-make:child-theme"));
-		this.log(chalk.green("\tyo wp-make:library"));
 	}
 });
 
